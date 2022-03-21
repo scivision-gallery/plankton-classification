b29e0b278375a71ea8c94a59e5216589b5474a83
Alejandro © <acocac@gmail.com>
2022-03-21 08:42:01 +0000

    add test

---
+++ tests/test_workflow.py
@@ -0,0 +1,17 @@
+import pytest
+
+def test_workflow():
+    from scivision import default_catalog, load_pretrained_model, load_dataset
+
+    model_name = 'resnet50-plantkton'
+    compatible_datasources = default_catalog.compatible_datasources(model_name).to_dataframe()
+
+    models = default_catalog.models.to_dataframe()
+    targetmodel = models[models.name == model_name].url.item()
+    model = load_pretrained_model(targetmodel, allow_install=True)
+
+    target_datasource = compatible_datasources.loc[compatible_datasources['name'] == 'data-004']
+    cat = load_dataset(target_datasource.url.item())
+    dataset = cat.plankton().to_dask()
+
+    model.predict(dataset, batch_size=26)
